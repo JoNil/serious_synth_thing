a95317494285e94947ecd3dfbeccb2eb1cda329c
Jonathan Nilsson <jonathan@voysys.se>
2017-03-31 23:35:53 +0200

    Struct

--- src/synth_thingy.rs
+++ src/synth_thingy.rs
@@ -7,6 +7,8 @@ pub struct Synth {
     tone_index: i32,
     counter: i32,
     bpm: i32,
+
+    envelope: Envelope,
 }
 
 impl Synth {
@@ -19,6 +21,7 @@ impl Synth {
             tone_index: 0,
             counter: 0,
             bpm: 120,
+            envelope: Envelope::new(),
         }
     }
 
@@ -38,6 +41,24 @@ impl Synth {
 
             *sample = (self.phase as f32 * self.tones[self.tone_index as usize] * 2.0 * 3.141592 / self.sample_rate as f32).sin();
             self.phase += 1;
+        }
+    }
+}
+
+pub struct Envelope {
+    attack: i32,
+    sustain: i32,
+    decay: i32,
+    release: i32,
+}
+
+impl Envelope {
+    pub fn new() -> Envelope {
+        Envelope {
+            attack: 0,
+            sustain: 0,
+            decay: 0,
+            release: 0,
         }
     }
 }
